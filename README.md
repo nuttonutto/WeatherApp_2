5081442d9fd438d68bc2d6d17883754607d43d21
nuttonutto <nuttototo@gmail.com>
2015-06-16 13:12:15 +0700

    Migrate to Android studio

---
+++ README.md
@@ -0,0 +1,4 @@
+# WeatherApp
+
+         WeatherApp is android weather application using OpenWeatherMap's current weather data API
+    to show current temperature of Chiang Mai, Thailand.
---
+++ app/src/main/java/com/nutto/weatherapp/WeatherActivity.java
@@ -0,0 +1,23 @@
+package com.nutto.weatherapp;
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+@SuppressWarnings("deprecation")
+public class WeatherActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_weather);
+		 
+	    if (savedInstanceState == null) {
+	        getSupportFragmentManager().beginTransaction()
+	                .add(R.id.container, new WeatherFragment())
+	                .commit();
+	    }
+	}
+	
+}
